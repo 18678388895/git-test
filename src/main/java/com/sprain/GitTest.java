945ea072cdd069def1f2fbe7e092588c3ed559c6
zhangchunyu <18678388895@163.com>
2024-01-13 10:52:56 +0800

    hot 1 commit

--- src/main/java/com/sprain/GitTest.java
+++ src/main/java/com/sprain/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.printf("git test");
         System.out.printf("git test2");
+        System.out.printf("git test3");
     }
 }
